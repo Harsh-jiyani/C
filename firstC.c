47271e72e33e23154045fbf50c0113314df7dc28
Harsh-jiyani <harshjiyani4277@gmail.com>
2024-12-17 12:02:43 +0530

    Add files via upload

---
+++ firstC.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a,b,q;
+printf("enter first number");
+scanf("%d\n",&a);
+ printf("enter second number");
+ scanf("%d\n",&b);
+  q=a+b;
+
+ printf("the sum of %d and %d is %d", a,b,q);
+    return 0;
+}
---
+++ secnd.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(){
+    int i;
+  i =  printf("jiyani %d kamleshbhai",printf("harsh"));
+printf ("%d",i);
+}
