ea9cc89eaac1fc98eec256d6baa952a0b113ea49
Harsh-jiyani <harshjiyani4277@gmail.com>
2024-12-18 19:29:14 +0530

    Add files via upload
    this 4th program

---
+++ 4.c
@@ -0,0 +1,14 @@
+// loop 
+// table 
+#include<stdio.h>
+void main(){
+
+    int i;
+    int n;
+    printf("enter number\n");
+    scanf("%d",&n);
+    for (i=1; i<=10;i++)
+    {
+        printf("%d\n",n*i);
+    }
+}
