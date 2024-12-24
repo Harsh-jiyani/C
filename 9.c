0dac313c5e4ca64e67aeac5100ae7162c9f1bfa1
Harsh-jiyani <harshjiyani4277@gmail.com>
2024-12-24 20:56:18 +0530

    pointer basic

---
+++ 9.c
@@ -0,0 +1,13 @@
+// pointer
+#include<stdio.h>
+int main()
+{
+    int x=300;
+    char*p;
+    p= &x;
+    printf("%d",*p);
+
+}
+// how to declear pointer 
+// int  *x =12|(" integer  is adrees")
+// char *x =12("charcter is adrees")
